19950a225f6a3c4f0c89ab3cefe83907cbd56d62
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-07-24 18:35:54 +0200

    [FEAT] SHOW VIEW

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,1 +1,13 @@
+@extends('layouts.app')
 
+@section('content')
+    <div class="container text-center my-4">
+        <h1>{{ $project->name }}</h1>
+        <a class="text-decoration-none text-black" href="{{ $project->git_link }}">GIT HUB PAGE</a>
+        @if ($project->finished)
+            <h6 class="text-success">Completato</h6>
+        @else
+            <h6 class="text-warning">In Lavorazione</h6>
+        @endif
+    </div>
+@endsection
